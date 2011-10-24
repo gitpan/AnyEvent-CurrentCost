0f3936f0cd2ac0894e1c8a630b43fe4ae36b0546
Mark Hindess <soft-cpan@temporalanomaly.com>
2011-10-24 21:24:12 +0000

    Import of BEANZ/AnyEvent-CurrentCost-1.112970 from CPAN.
    gitpan-cpan-distribution: AnyEvent-CurrentCost
    gitpan-cpan-version:      1.112970
    gitpan-cpan-path:         BEANZ/AnyEvent-CurrentCost-1.112970.tar.gz
    gitpan-cpan-author:       BEANZ
    gitpan-cpan-maturity:     released

--- lib/AnyEvent/CurrentCost.pm
+++ lib/AnyEvent/CurrentCost.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package AnyEvent::CurrentCost;
 BEGIN {
-  $AnyEvent::CurrentCost::VERSION = '1.110792';
+  $AnyEvent::CurrentCost::VERSION = '1.112970';
 }
 
 # ABSTRACT: AnyEvent module for reading from Current Cost energy meters
@@ -104,7 +104,7 @@ AnyEvent::CurrentCost - AnyEvent module for reading from Current Cost energy met
 
 =head1 VERSION
 
-version 1.110792
+version 1.112970
 
 =head1 SYNOPSIS
 
